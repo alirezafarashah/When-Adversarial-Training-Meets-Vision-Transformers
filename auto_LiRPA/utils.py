65f7a9798e76de576ca7b9e071c57f955a45f9a4
alirezafarashah <63104907+alirezafarashah@users.noreply.github.com>
2023-05-11 13:06:12 +0330

    Update utils.py

--- auto_LiRPA/utils.py
+++ auto_LiRPA/utils.py
@@ -6,7 +6,8 @@ import os
 import sys
 import appdirs
 from oslo_concurrency import lockutils, processutils
-from collections.abc import defaultdict, Sequence, namedtuple
+from collections import defaultdict, namedtuple
+from collections.abc import Sequence
 
 logging.basicConfig(
     format='%(levelname)-8s %(asctime)-12s %(message)s',
